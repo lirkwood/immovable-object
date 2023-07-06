a8a8d0a8225b36983d5ec50cd6ad7336ddf943e8
Linus Kirkwood <linuskirkwood@gmail.com>
2023-07-06 16:45:31 +1000

    Fixed remote control webpage

--- src/remote.rs
+++ src/remote.rs
@@ -4,6 +4,7 @@ use gotham::pipeline::{single_middleware, single_pipeline};
 use gotham::prelude::*;
 use gotham::router::build_router;
 use gotham::state::State;
+use tempfile::NamedTempFile;
 use std::sync::{Arc, Mutex, MutexGuard};
 
 #[derive(Clone, StateData)]
@@ -46,6 +47,7 @@ impl Drivable for CarControl {
 }
 
 pub fn enable(mut state: State) -> (State, String) {
+    println!("Enabling...");
     let car = CarControl::borrow_mut_from(&mut state);
     car.enable();
 
@@ -53,6 +55,7 @@ pub fn enable(mut state: State) -> (State, String) {
 }
 
 pub fn disable(mut state: State) -> (State, String) {
+    println!("Disabling...");
     let car = CarControl::borrow_mut_from(&mut state);
     car.disable();
 
@@ -60,19 +63,31 @@ pub fn disable(mut state: State) -> (State, String) {
 }
 
 pub fn serve(car: CarControl) {
-    let middleware = StateMiddleware::new(car);
+    let index_file = NamedTempFile::new().unwrap();
+    std::fs::write(&index_file, LANDING_PAGE).unwrap();
 
-    // create a middleware pipeline from our middleware
-    let pipeline = single_middleware(middleware);
-
-    // construct a basic chain from our pipeline
-    let (chain, pipelines) = single_pipeline(pipeline);
-
-    // build a router with the chain & pipeline
+    let (chain, pipelines) = single_pipeline(single_middleware(StateMiddleware::new(car)));
     let router = build_router(chain, pipelines, |route| {
-        route.get("/start").to(enable);
-        route.get("/stop").to(disable);
+        route.get("/").to_file(index_file.path());
+        route.post("/start").to(enable);
+        route.post("/stop").to(disable);
     });
 
-    gotham::start("localhost:80", router).unwrap();
+    gotham::start("0.0.0.0:80", router).unwrap();
 }
+
+const LANDING_PAGE: &'static str = "<!DOCTYPE html><html>
+<head>
+    <h1>Immovable Object Controls</h1>
+</head>
+<body>
+	<div style=\"display:flex\">
+        <form action=\"/start\" method=\"post\" target=\"dummy\">
+            <button name=\"start\" value=\"start\" style=\"padding:20px;margin:50px\">start</button>
+        </form>
+        <form action=\"/stop\" method=\"post\" target=\"dummy\">
+            <button name=\"stop\" value=\"stop\" style=\"padding:20px;margin:50px\">stop</button>
+        </form>
+        <iframe name=\"dummy\" id=\"dummy\" hidden/>
+	</div>
+</body></html>";
